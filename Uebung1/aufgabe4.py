0df842b69a36adfc345d92bf7d6fae2719a8a4d1
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2023-10-01 17:02:47 +0200

    finished first working build

--- Uebung1/aufgabe4.py
+++ Uebung1/aufgabe4.py
@@ -1,7 +1,8 @@
 from matplotlib import pyplot as plt
 from scipy import io as sio
+from scipy import interpolate as sint
 import pandas as pd
-import numpy as np
+# import numpy as np
 
 
 filename = "mkl.mat"
@@ -9,7 +10,11 @@ file = sio.loadmat(filename, struct_as_record=False, squeeze_me=True)
 x_value = file["x_werte"]
 y_value = file["y_werte"]
 data = [x_value, y_value]
-data_df = pd.DataFrame(data=data).transpose()
 
-plt.plot(data_df[0],data_df[1])
+data_df = pd.DataFrame(data=data).transpose()
+# plt.plot(data_df[0], data_df[1], "o")
+
+spline = sint.CubicSpline(data_df[0], data_df[1])
+plt.plot(data_df[0], spline(data_df[0]))
+
 plt.show()
