61c1ef365912585cade51a43aa7208d8d9396cb3
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2023-10-18 16:43:55 +0200

    added launch.json to run the files

--- exercise 2/aufgabe1.py
+++ exercise 2/aufgabe1.py
@@ -1,20 +1,2 @@
-from OMPython import OMCSessionZMQ
-omc = OMCSessionZMQ()
-
-# cmds = [
-# 'loadFile(getInstallationDirectoryPath() + "/share/doc/omc/testmodels/BouncingBall.mo")',
-# "simulate(BouncingBall)",
-# "plot(h)"
-# ]
-# for cmd in cmds:
-#     answer = omc.sendExpression(cmd)
-#     print("\n{}:\n{}".format(cmd, answer))
-
-# from OMPython import OMCSessionZMQ
-# # omc = OMCSessionZMQ()
-# # model_path=omc.sendExpression("getInstallationDirectoryPath()") + "/share/doc/omc/testmodels/"
-
-# from OMPython import ModelicaSystem
-# ModelicaSystem(useCorba=True)
-# mod=ModelicaSystem(model_path + "BouncingBall.mo","BouncingBall",["Modelica"])
-
+from OMPython import ModelicaSystem
+import matplotlib.pyplot as plt
--- exercise 2/pt1_step.py
+++ exercise 2/pt1_step.py
@@ -7,12 +7,12 @@ ModelicaSystem(useCorba=False)
 modelname='pt1_step'
 mod=ModelicaSystem(modelname+'.mo',modelname)
 mod.setSimulationOptions('stopTime=5.0')
-#mod.setParameters('firstOrder1.k=2.0')
+mod.setParameters('firstOrder1.k=2.0')
 mod.simulate()
 
 [t]=mod.getSolutions('time')
 [phi]=mod.getSolutions('firstOrder1.y')
-delete_OM_files(modelname)
+# delete_OM_files(modelname)
 
 fig=plt.figure(1, figsize=(10,6)); fig.clf()
 plt.plot(t, phi, 'b')
